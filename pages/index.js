1acbac35086836d578f895ab3200a72f8854fb82
heitor-canhizares <heitorcanhizares@gmail.com>
2022-10-18 22:01:29 -0400

    Melhoramento do código

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,41 @@
-import Head from 'next/head'
+import dayjs from "dayjs";
+import useSWR from "swr";
+
+const fetcher = url => fetch(url).then(res => res.json());
+
+const tableRowItem = function (item) {
+    return (
+        <tr>
+            <td>{item.id}</td>
+            <td>{item.title}</td>
+            <td>{item.description}</td>
+            <td>{dayjs(item.createdAt).format("DD/MM/YYYY HH:mm")}</td>
+            <td>{item.done ? "Sim" : "Não"}</td>
+            <td>{item.finishedAt}</td>
+        </tr>
+    );
+};
 
 export default function Home() {
-  return (
-    <div>
-      <Head>
-        <title>To-Do</title>
-        <meta name="description" content="To-Do NextJS" />
-      </Head>
-      <h1>Olá mundo</h1>
-    </div>
-  )
+    const { data, error } = useSWR("/api/todos", fetcher);
+
+    if (!data) return <div>Carregando...</div>;
+
+    return (
+        <div>
+            <table>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Título</th>
+                        <th>Descrição</th>
+                        <th>Criado Em</th>
+                        <th>Finalizado</th>
+                        <th>Finalizado Em</th>
+                    </tr>
+                </thead>
+                <tbody>{data.map(item => tableRowItem(item))}</tbody>
+            </table>
+        </div>
+    );
 }
