bedc6c88b58312d4be64a74faaa02262dcb017a8
heitor-canhizares <heitorcanhizares@gmail.com>
2022-11-29 19:03:25 -0400

    Atualições da ultima aula

--- components/TodoDialog.js
+++ components/TodoDialog.js
@@ -1,61 +1,59 @@
-/* eslint-disable react-hooks/rules-of-hooks */
-import { useState } from "react";
+import { useState, useEffect } from 'react';
+import { useSWRConfig } from 'swr'
 
-const postNewTodo = function (title, description, todo) {
-    todo.title = title;
-    todo.description = description;
-    fetch("/api/todos", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(todo),
-    });
-};
+const updateTodo = (title, description, id, mutate) => {
+    mutate('/api/todos', async todos => {
+        const todo = { id, title, description }
+
+        let result = await fetch('/api/todos', {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(todo)
+        })
+
+        let updatedTodo = await result.json()
+        let index = todos.map((i) => { return i.id }).indexOf(id)
+        todos[index] = updatedTodo;
+        return todos;
+    })
+
+}
 
 export default function TodoDialog(props) {
-    if (props.isOpen === true) {
-        const [title, setTitle] = useState(props.item.title);
-        const [description, setDescription] = useState(props.item.description);
+    const { title, description } = props.item;
+    const { mutate } = useSWRConfig()
 
-        return (
-            <div className="todo-dialog">
-                <div className="todo-dialog-toolbar">
-                    <div>{props.title}</div>
-                    <div
-                        onClick={() => props.setIsOpen(false)}
-                        className="close"
-                    >
-                        X
-                    </div>
-                </div>
-                <div className="todo-dialog-body">
-                    <input
-                        className="title-input"
-                        type="text"
-                        placeholder="Título"
-                        value={title}
-                        onChange={v => setTitle(v.target.value)}
-                    />
-                    <textarea
-                        className="description-input"
-                        type="text"
-                        placeholder="Descrição"
-                        value={description}
-                        onChange={v => setDescription(v.target.value)}
-                    />
-                    <div className="action-buttons">
-                        <button
-                            onClick={() => {
-                                updateTodo(title, description, props.item);
-                                props.setIsOpen(false);
-                            }}
-                        >
-                            Salvar
-                        </button>
-                    </div>
-                </div>
-            </div>
-        );
-    }
+    const [inputTitle, setInputTitle] = useState("")
+    const [inputDescription, setInputDescription] = useState("")
+
+    useEffect(() => {
+        title ? setInputTitle(title) : setInputTitle("")
+        description ? setInputDescription(description) : setInputDescription("")
+    }, [title, description])
+
+    return (
+        <>
+            {
+                props.isOpen ?
+                    <div className='todo-dialog'>
+                        <div className='todo-dialog-toolbar'>
+                            <div>{props.title}</div>
+                            <div onClick={() => props.setIsOpen(false)} className='close'>X</div>
+                        </div>
+                        <div className="todo-dialog-body">
+                            <input className='title-input' type="text" placeholder="Título" value={inputTitle} onChange={e => setInputTitle(e.target.value)} />
+                            <textarea className='description-input' type="text" placeholder="Descrição" value={inputDescription} onChange={e => setInputDescription(e.target.value)} />
+                            <div className="action-buttons">
+                                <button onClick={() => {
+                                    updateTodo(inputTitle, inputDescription, props.item.id, mutate)
+                                    props.setIsOpen(false)
+                                }}>Salvar</button>
+                            </div>
+                        </div>
+                    </div> : <></>
+            }
+        </>
+    )
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,8 @@
-import "../styles/globals.css";
-import "../styles/TodoDialog.css";
+import '../styles/globals.css'
+import '../styles/TodoDialog.css'
 
 function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />;
+  return <Component {...pageProps} />
 }
 
-export default MyApp;
+export default MyApp
