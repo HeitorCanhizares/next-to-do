7a588158c15bf179b940a9eb10cd61448c7bd826
heitor-canhizares <heitorcanhizares@gmail.com>
2022-11-29 19:16:02 -0400

    outras atts

--- components/TodoDialog.js
+++ components/TodoDialog.js
@@ -1,59 +1,91 @@
-import { useState, useEffect } from 'react';
-import { useSWRConfig } from 'swr'
+import { useState, useEffect } from "react";
+import { useSWRConfig } from "swr";
 
 const updateTodo = (title, description, id, mutate) => {
-    mutate('/api/todos', async todos => {
-        const todo = { id, title, description }
+	mutate("/api/todos", async todos => {
+		const todo = { id, title, description };
 
-        let result = await fetch('/api/todos', {
-            method: 'PUT',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(todo)
-        })
+		let result = await fetch("/api/todos", {
+			method: "PUT",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(todo),
+		});
 
-        let updatedTodo = await result.json()
-        let index = todos.map((i) => { return i.id }).indexOf(id)
-        todos[index] = updatedTodo;
-        return todos;
-    })
-
-}
+		let updatedTodo = await result.json();
+		let index = todos
+			.map(i => {
+				return i.id;
+			})
+			.indexOf(id);
+		todos[index] = updatedTodo;
+		return todos;
+	});
+};
 
 export default function TodoDialog(props) {
-    const { title, description } = props.item;
-    const { mutate } = useSWRConfig()
+	const { title, description } = props.item;
+	const { mutate } = useSWRConfig();
 
-    const [inputTitle, setInputTitle] = useState("")
-    const [inputDescription, setInputDescription] = useState("")
+	const [inputTitle, setInputTitle] = useState("");
+	const [inputDescription, setInputDescription] = useState("");
 
-    useEffect(() => {
-        title ? setInputTitle(title) : setInputTitle("")
-        description ? setInputDescription(description) : setInputDescription("")
-    }, [title, description])
+	useEffect(() => {
+		title ? setInputTitle(title) : setInputTitle("");
+		description
+			? setInputDescription(description)
+			: setInputDescription("");
+	}, [title, description]);
 
-    return (
-        <>
-            {
-                props.isOpen ?
-                    <div className='todo-dialog'>
-                        <div className='todo-dialog-toolbar'>
-                            <div>{props.title}</div>
-                            <div onClick={() => props.setIsOpen(false)} className='close'>X</div>
-                        </div>
-                        <div className="todo-dialog-body">
-                            <input className='title-input' type="text" placeholder="Título" value={inputTitle} onChange={e => setInputTitle(e.target.value)} />
-                            <textarea className='description-input' type="text" placeholder="Descrição" value={inputDescription} onChange={e => setInputDescription(e.target.value)} />
-                            <div className="action-buttons">
-                                <button onClick={() => {
-                                    updateTodo(inputTitle, inputDescription, props.item.id, mutate)
-                                    props.setIsOpen(false)
-                                }}>Salvar</button>
-                            </div>
-                        </div>
-                    </div> : <></>
-            }
-        </>
-    )
+	return (
+		<>
+			{props.isOpen ? (
+				<div className="todo-dialog">
+					<div className="todo-dialog-toolbar">
+						<div>{props.title}</div>
+						<div
+							onClick={() => props.setIsOpen(false)}
+							className="close"
+						>
+							X
+						</div>
+					</div>
+					<div className="todo-dialog-body">
+						<input
+							className="title-input"
+							type="text"
+							placeholder="Título"
+							value={inputTitle}
+							onChange={e => setInputTitle(e.target.value)}
+						/>
+						<textarea
+							className="description-input"
+							type="text"
+							placeholder="Descrição"
+							value={inputDescription}
+							onChange={e => setInputDescription(e.target.value)}
+						/>
+						<div className="action-buttons">
+							<button
+								onClick={() => {
+									updateTodo(
+										inputTitle,
+										inputDescription,
+										props.item.id,
+										mutate
+									);
+									props.setIsOpen(false);
+								}}
+							>
+								Salvar
+							</button>
+						</div>
+					</div>
+				</div>
+			) : (
+				<></>
+			)}
+		</>
+	);
 }
--- components/todoForm.js
+++ components/todoForm.js
@@ -1,58 +1,75 @@
-import { useState } from 'react';
-import { useSWRConfig } from 'swr'
+import { useState } from "react";
+import { useSWRConfig } from "swr";
 
 const postNewTodo = function (todo, mutate) {
-    mutate('/api/todos', async todos => {
-        let result = await fetch('/api/todos', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(todo),
-        })
-        let insertedTodo = await result.json()
-        let filteredData = todos.filter(todo => todo.id !== insertedTodo.id)
-        return [...filteredData, insertedTodo]
-    }, { revalidate: false })
-}
+	mutate(
+		"/api/todos",
+		async todos => {
+			let result = await fetch("/api/todos", {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify(todo),
+			});
+			let insertedTodo = await result.json();
+			let filteredData = todos.filter(
+				todo => todo.id !== insertedTodo.id
+			);
+			return [...filteredData, insertedTodo];
+		},
+		{ revalidate: false }
+	);
+};
 
 const checkKeyPressed = function (event, callbackFunc) {
-    if (event.defaultPrevented) return;
-    let key = event.key || event.keyCode;
-    if (key === 'Enter' || key === 13) callbackFunc();
-}
+	if (event.defaultPrevented) return;
+	let key = event.key || event.keyCode;
+	if (key === "Enter" || key === 13) callbackFunc();
+};
 
 export default function TodoForm(props) {
+	const { mutate } = useSWRConfig();
 
-    const { mutate } = useSWRConfig()
+	const [title, setTitle] = useState("");
+	const [description, setDescription] = useState("");
 
-    const [title, setTitle] = useState("")
-    const [description, setDescription] = useState("")
+	return (
+		<div className="todo-form input-group">
+			<input
+				className="title-input"
+				type="text"
+				placeholder="O que você precisa fazer?"
+				value={title}
+				onChange={e => setTitle(e.target.value)}
+				onKeyUp={e =>
+					checkKeyPressed(e, () => {
+						postNewTodo({ title, description }, mutate);
+						setTitle("");
+					})
+				}
+			/>
 
-    return (
-        <div className='todo-form input-group'>
-            <input className='title-input'
-                type="text"
-                placeholder="O que você precisa fazer?"
-                value={title}
-                onChange={e => setTitle(e.target.value)}
-                onKeyUp={(e) => checkKeyPressed(e, () => {
-                    postNewTodo({ title, description }, mutate)
-                    setTitle("")
-                })} />
-
-
-            <button className='add-button' onClick={() => {
-                postNewTodo({ title, description }, mutate)
-                setTitle("")
-            }}>
-
-                <svg width="20px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
-                    <path fillRule="evenodd" d="M12 3.75a.75.75 0 01.75.75v6.75h6.75a.75.75 0 010 1.5h-6.75v6.75a.75.75 0 01-1.5 0v-6.75H4.5a.75.75 0 010-1.5h6.75V4.5a.75.75 0 01.75-.75z" clipRule="evenodd" />
-                </svg>
-
-            </button>
-
-        </div>
-    )
+			<button
+				className="add-button"
+				onClick={() => {
+					postNewTodo({ title, description }, mutate);
+					setTitle("");
+				}}
+			>
+				<svg
+					width="20px"
+					xmlns="http://www.w3.org/2000/svg"
+					viewBox="0 0 24 24"
+					fill="currentColor"
+				>
+					<path
+						fillRule="evenodd"
+						d="M12 3.75a.75.75 0 01.75.75v6.75h6.75a.75.75 0 010 1.5h-6.75v6.75a.75.75 0 01-1.5 0v-6.75H4.5a.75.75 0 010-1.5h6.75V4.5a.75.75 0 01.75-.75z"
+						clipRule="evenodd"
+					/>
+				</svg>
+			</button>
+		</div>
+	);
 }
